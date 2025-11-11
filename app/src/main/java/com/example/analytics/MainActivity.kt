d761e24aab392438a135f4164eb2f32cac04b32c
CarlesPontacq <carles.pontacq@estudiant.enti.cat>
2025-11-11 11:38:35 +0100

    Made crash button

--- app/src/main/java/com/example/analytics/MainActivity.kt
+++ app/src/main/java/com/example/analytics/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.analytics
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var analytics: FirebaseAnalytics
+    private lateinit var crashButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,11 +21,15 @@ class MainActivity : AppCompatActivity() {
             putString("portrait_orientation", (resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT).toString())
         }
 
+        crashButton = findViewById(R.id.crashButton)
+        crashButton.setOnClickListener{onButtonClick()}
+
 
 
         analytics.logEvent("MyFirstEvent", null)
+    }
 
-        val crashArray = intArrayOf(1, 2)
-        crashArray[2]
+    private fun onButtonClick(){
+        throw RuntimeException("Mensaje")
     }
 }
